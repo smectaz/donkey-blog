49f9cd423f9e79c2b5377f135b4c34baeb0d9db2
smectaz <90684643+smectaz@users.noreply.github.com>
2021-12-22 12:26:30 +0100

    installation bootstrap

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ArticleRepository;
 use Knp\DoctrineBehaviors\Model\Sluggable\SluggableTrait;
 use Knp\DoctrineBehaviors\Contract\Entity\SluggableInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article implements SluggableInterface
@@ -24,6 +25,7 @@ class Article implements SluggableInterface
     private $content;
 
     #[ORM\Column(type: 'datetime', nullable: true)]
+    #[Assert\DateTime]
     private $createdDate;
 
     #[ORM\ManyToOne(targetEntity: author::class, inversedBy: 'articles')]
