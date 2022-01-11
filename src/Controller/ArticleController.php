796c7367cff6aeed904377026f91351738ad72c7
smectaz <90684643+smectaz@users.noreply.github.com>
2022-01-11 17:56:05 +0100

    install pagination

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 
 use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,15 +13,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticleController extends AbstractController
 {
     #[Route('/articles', name: 'articles')]
-    public function list( ArticleRepository $articleRepository): Response
+    public function list( ArticleRepository $articleRepository, Request $request, PaginatorInterface $paginatorInterface): Response
     {
-        $listArticle = $articleRepository->findAll();
-       
+        //$listArticle = $articleRepository->findAll();
+        $datas = $articleRepository->findBy([], ['id' => 'asc']);
+        $articles = $paginatorInterface->paginate(
+        $datas,
+        $request->query->getInt('page',1),
+        6
+        );
         
 
         return $this->render('article/article.html.twig', [
             'controller_name' => 'ArticleController',
-            'listeArticles' => $listArticle,
+            'pagination' => $articles,
         ]);
     }
 }
