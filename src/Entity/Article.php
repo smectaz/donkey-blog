01bb504484f72cf0d0805083156e28a297c0bb83
smectaz <90684643+smectaz@users.noreply.github.com>
2022-01-14 15:55:18 +0100

    upload image avec api

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -4,10 +4,19 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ArticleRepository;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Knp\DoctrineBehaviors\Model\Sluggable\SluggableTrait;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use Knp\DoctrineBehaviors\Contract\Entity\SluggableInterface;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read_tag' ]],
+    denormalizationContext: ['groups' => ['write_tag']]
+)]
+#[ApiFilter(OrderFilter::class, properties: ['id', 'title'], arguments: ['orderParameterName' => 'order'])]
 class Article implements SluggableInterface
 {
     use SluggableTrait;
@@ -15,9 +24,11 @@ class Article implements SluggableInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read_tag'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read_tag', 'write_tag'])]
     private $title;
 
     #[ORM\Column(type: 'string', length: 255)]
@@ -34,6 +45,9 @@ class Article implements SluggableInterface
 
     #[ORM\ManyToOne(targetEntity: Commentary::class, inversedBy: 'articles')]
     private $commentary;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
 
     public function getId(): ?int
     {
@@ -118,6 +132,18 @@ class Article implements SluggableInterface
     public function setCommentary(?commentary $commentary): self
     {
         $this->commentary = $commentary;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
