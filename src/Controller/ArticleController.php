9c039a05724394b1b404459a5137cca18f89ea25
smectaz <90684643+smectaz@users.noreply.github.com>
2022-02-07 10:40:45 +0100

    correction

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,31 +2,28 @@
 
 namespace App\Controller;
 
-
 use App\Repository\ArticleRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/articles', name: 'articles')]
-    public function list( ArticleRepository $articleRepository, Request $request, PaginatorInterface $paginatorInterface): Response
-    {
-        //$listArticle = $articleRepository->findAll();
-        $datas = $articleRepository->findBy([], ['id' => 'asc']);
-        $articles = $paginatorInterface->paginate(
+    #[Route('/articles', name:'articles')]
+function list(ArticleRepository $articleRepository, Request $request, PaginatorInterface $paginatorInterface): Response {
+    //$listArticle = $articleRepository->findAll();
+    $datas = $articleRepository->findBy([], ['id' => 'asc']);
+    $articles = $paginatorInterface->paginate(
         $datas,
-        $request->query->getInt('page',1),
+        $request->query->getInt('page', 1),
         6
-        );
-        
+    );
 
-        return $this->render('article/article.html.twig', [
-            'controller_name' => 'ArticleController',
-            'pagination' => $articles,
-        ]);
-    }
+    return $this->render('article/article.html.twig', [
+        'controller_name' => 'ArticleController',
+        'pagination' => $articles,
+    ]);
+}
 }
